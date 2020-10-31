8f4515a519f79e5b8cd8a0fff0a000d578b51c63
embong <chernomega11@gmail.com>
2020-10-31 19:20:36 +0700

    Added custom recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,8 @@ from core.models import Tag, Ingredient, Recipe
 from recipe.serializers import (
     TagSerializer,
     IngredientSerializer,
-    RecipeSerializer
+    RecipeSerializer,
+    RecipeDetailSerializer
 )
 
 
@@ -63,3 +64,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         Retrieve the recipes for the authenticated user
         """
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """
+        Return appropriate serializer class
+        """
+        if self.action == 'retrieve':
+            return RecipeDetailSerializer
+
+        return self.serializer_class
